95cfa4621dfe2a6da96f21464e714ea05e03bbbd
wqk <1249955391@qq.com>
2017-07-18 01:09:14 +0800

    test

--- servletTest/src/com/java/servlet/DownloadServlet.java
+++ servletTest/src/com/java/servlet/DownloadServlet.java
@@ -13,7 +13,7 @@ import javax.servlet.http.HttpServletResponse;
 
 
 public class DownloadServlet extends HttpServlet {
-
+  
 	public void doGet(HttpServletRequest request,HttpServletResponse response) throws ServletException, IOException{
 		doPost(request, response);
 	}
